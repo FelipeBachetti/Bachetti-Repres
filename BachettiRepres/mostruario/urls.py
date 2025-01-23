86a6f553c6daea85441ec9828b52d9a0de27967b
Felipe Bachetti <84641892+FelipeBachetti@users.noreply.github.com>
2025-01-23 11:22:25 -0300

    product_page

--- BachettiRepres/mostruario/urls.py
+++ BachettiRepres/mostruario/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     path("", views.home, name="home"),
-    path("empresa/<int:id>/", views.company_page, name="company" )
+    path("empresa/<int:id>/", views.company_page, name="company" ),
+    path("empresa/<int:id_emp>/tipo/<int:id_tip>/", views.product_page, name="product"),
 ]
