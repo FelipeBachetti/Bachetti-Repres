99f81a56b994d03b2c487aeb8dcd65a22c21fd50
Felipe Bachetti <84641892+FelipeBachetti@users.noreply.github.com>
2025-01-31 12:44:35 -0300

    carousel

--- BachettiRepres/mostruario/models.py
+++ BachettiRepres/mostruario/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.postgres.fields import ArrayField
 
 class Empresa(models.Model):
     nome = models.CharField(max_length=80)
@@ -19,7 +20,8 @@ class Produto(models.Model):
     nome = models.CharField(max_length=80)
     descricao = models.CharField(max_length=200, null=True, blank=True)
     tipo = models.ForeignKey('Tipo_Produto',on_delete=models.CASCADE,)
-    link_imagem = models.CharField(max_length=200, null=True, blank=True)
+    link_imagem_principal = models.CharField(max_length=200, null=True, blank=True)
+    link_imagens_extras = ArrayField(models.CharField(max_length=200), blank=True, default=list)
 
     def __str__(self):
         return self.nome
