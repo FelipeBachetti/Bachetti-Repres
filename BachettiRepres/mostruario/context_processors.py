51ee12286d83d1fb1235bcc30566ff22192fccf0
Felipe Bachetti <84641892+FelipeBachetti@users.noreply.github.com>
2025-02-18 08:06:00 -0300

    finishing

--- BachettiRepres/mostruario/context_processors.py
+++ BachettiRepres/mostruario/context_processors.py
@@ -1,5 +1,5 @@
 from .models import Empresa
 
 def empresas_context(request):
-    empresas = Empresa.objects.all().values()  # Consulta as empresas
+    empresas = Empresa.objects.prefetch_related("tipo_produto_set").all()
     return {'empresas': empresas}
